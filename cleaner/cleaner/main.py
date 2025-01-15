6f825e7521ee5ae7799253b86e91dd13d1cb46ff
xpakx <55586470+xpakx@users.noreply.github.com>
2025-01-15 22:02:03 +0100

    feat: update cleaner script for scenario with multiple years

--- cleaner/cleaner/main.py
+++ cleaner/cleaner/main.py
@@ -1,11 +1,28 @@
 import os
 import json
+import argparse
 
+def load_ids(filename: str):
+    with open(filename) as f:
+        albums = json.load(f)
+    return {album['id'] for album in albums}
 
 def main():
-    with open('dist/albumy.json') as f:
-        albums = json.load(f)
-    ids = {album['id'] for album in albums}
+    parser = argparse.ArgumentParser(description="Album cover cleaner")
+    parser.add_argument( 'files', metavar='FILE', type=str, nargs='+', help='List of files with album data.')
+
+    args = parser.parse_args()
+    file_list = args.files
+    if len(file_list) == 0:
+        print("No files with album data. Aborting.")
+        return
+
+    print(file_list)
+    ids = []
+    for filename in file_list:
+        print(f"Loading file: {filename}")
+        ids.extend(load_ids(filename))
+
     dir = 'dist/'
 
     for filename in os.listdir(dir):
